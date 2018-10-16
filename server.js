43096a6c6fe34813ae382ce8e488e9a7bc4ed4f7
GeoffroyBERNARD <geof.b@laposte.net>
2018-10-16 23:36:37 +0200

    Project initialization

---
+++ server.js
@@ -0,0 +1,24 @@
+//loading modules
+const express = require('express')
+const app = express()
+
+//defining the view engine
+app.set('view engine', 'ejs');
+
+//defining network
+const ip = "0.0.0.0";
+const port = "80";
+
+//defining ressources folder
+app.use(express.static(__dirname + '/public'));
+
+//main view
+app.use("/", (req, res) => {
+    res.render("main");
+    console.log("Request from " + req.ip)
+});
+
+//listening
+app.listen(port, ip, function () {
+  console.log('Server listening on ' + ip + ' with port ' + port)
+})
